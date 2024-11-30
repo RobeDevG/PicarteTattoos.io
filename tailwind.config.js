892b6ce455fa595b75f8db99b9fed9adba89d28a
RobeDevG <robertodev97@gmail.com>
2024-11-30 12:50:00 -0500

    some changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,18 +6,6 @@ export default {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'bg-header-mobile': "url('./src/assets/Tattoo Studio.webp')",
-        'bg-header-desk': "url('./src/assets/Tattoo Studio.webp')",
-        'bg-Primary':"url('./src/assets/BgPrimary.webp')",
-        'bg-Banner':"url('./src/assets/Banner.webp')",
-        'bg-Review':"url('./src/assets/Review.webp')",
-        'bg-Banner_2':"url('./src/assets/Banner_2.webp')",
-        'bg-Study':"url('./src/assets/bgstudy.webp')",
-        'bg-Care':"url('./src/assets/BannerCuidados.webp')",
-        'bg-Contact':"url('./src/assets/BannerContact.webp')"
-
-      },
       fontFamily:{
         "custom-font": ["Kenia", "sans-serif"],
         "font-slider":["ABeeZee", "sans-serif"],
