1096c2a2fb82648a4726a8e4168e685a5917cde0
RobeDevG <robertodev97@gmail.com>
2024-11-30 12:50:09 -0500

    es asi

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './bg.css'
 import {I18nextProvider} from 'react-i18next'
 import i18next from 'i18next'
 
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: { chunkSizeWarningLimit: 1000, rollupOptions: { output: { manualChunks: { vendor: ['react', 'react-dom'] } } } }
+  build: { chunkSizeWarningLimit: 1000, rollupOptions: { output: { manualChunks: { vendor: ['react', 'react-dom'] } } } },
+  assetsInclude: ['**/*.webp']
 });
 
 
