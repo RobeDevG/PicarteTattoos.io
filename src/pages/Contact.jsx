52b3fa71b6ab0ede808146fbc747eaaa1f4e6371
RobeDevG <robertodev97@gmail.com>
2024-12-04 20:32:28 -0500

    somechangesresponsive

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -19,7 +19,7 @@ function Contact() {
         <p className='font-font-Kelly text_shadow_B text-white numbs_2 text-4xl mt-11'>{t("Contact.text_1")} <span className='span-red'> {t("Contact.span_1")} </span></p>
         <p className='font-font-Kelly text_shadow_B text-white numbs_2 text-4xl'>  {t("Contact.text_2")} <span className='span-red'> {t("Contact.span_2")} </span> {t("Contact.text_3")} </p>
       </section>
-      <section className='bg-[#151515] flex xl:flex-row flex-col justify-center items-center xl:items-start  text-white pb-6'>
+      <section className='bg-[#151515] flex xl:flex-row flex-col justify-center items-center xl:items-start text-center text-white pb-6'>
         <div className='flex flex-col lg:w-2/4 items-center'>
           <h2 className='font-custom-font text-5xl mt-3'>{t("Contact.title_1")}</h2>
           <p className='font-font-slider text-2xl mt-3'>{t("Contact.des_1")}</p>
@@ -37,21 +37,21 @@ function Contact() {
           </div>
 
         </div>
-        <div className='flex flex-col xl:items-stretch items-center justify-center xl:justify-stretch lg:w-2/4 text-white'>
+        <div className='flex flex-col xl:items-stretch items-center justify-center xl:justify-stretch lg:w-2/4 text-white text-center sm:text-start'>
           <h2 className='font-custom-font text-5xl mt-3 '>{t("Contact.title_form")}</h2>
           <p className='font-font-slider text-2xl mt-10 max-w-96 xl:max-w-xl'>{t("Contact.des_form")}</p>
           <form className='flex flex-col xl:items-stretch items-center justify-center xl:justify-stretch text-white' action="">
-            <label htmlFor="name" className='font-font-slider text-2xl mt-24'>{t("Contact.label_1")}</label>
-            <input type="text" id='name' className='bg-[#262626] box-shadowimp w-[300px] sm:w-[] h-20 mt-3 text-4xl' />
+            <label htmlFor="name" className='font-font-slider text-2xl mt-24 '>{t("Contact.label_1")}</label>
+            <input type="text" id='name' className='bg-[#262626] box-shadowimp w-[300px] sm:w-[600px] h-20 mt-3 text-4xl' />
 
             <label htmlFor="email" className='font-font-slider text-2xl mt-14'>{t("Contact.label_2")}</label>
-            <input type="email" id='email' className='bg-[#262626] box-shadowimp numbs_3 h-20 mt-3 text-4xl' />
+            <input type="email" id='email' className='bg-[#262626] box-shadowimp w-[300px] sm:w-[600px] h-20 mt-3 text-4xl' />
 
             <label htmlFor="subject" className='font-font-slider text-2xl mt-14'>{t("Contact.label_3")}</label>
-            <input type="text" id='subject' className='bg-[#262626] box-shadowimp numbs_3 h-20 mt-3 text-4xl' />
+            <input type="text" id='subject' className='bg-[#262626] box-shadowimp w-[300px] sm:w-[600px] h-20 mt-3 text-4xl' />
 
             <label htmlFor="message" className='font-font-slider text-2xl mt-14'>{t("Contact.label_4")}</label>
-            <textarea type="text" id='message' className='bg-[#262626] box-shadowimp numbs_3 h-64 mt-3 text-4xl resize-none' />
+            <textarea type="text" id='message' className='bg-[#262626] box-shadowimp w-[300px] sm:w-[600px] h-64 mt-3 text-4xl resize-none' />
 
             <button className='bg-[#940B0B] w-40 h-12 font-custom-font text-4xl mt-7'>Enviar</button>
 
